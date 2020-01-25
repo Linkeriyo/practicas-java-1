e2b1c5251fb4e9f830ee011c7757bdb936203f5a
Linkeriyo <linkeriyo@gmail.com>
2020-01-25 05:17:40 +0100

    different constructors implemented

--- El_Juego_De_La_Vida/src/el_juego_de_la_vida/El_Juego_De_La_Vida.java
+++ El_Juego_De_La_Vida/src/el_juego_de_la_vida/El_Juego_De_La_Vida.java
@@ -46,48 +46,62 @@ public class El_Juego_De_La_Vida {
         char option;
         do {
             String input = in.next().toUpperCase();
-            int width, height, numberAlive;
+
             option = input.charAt(0);
             switch (option) {
                 case 'S':
                     break;
                 default:
                     System.out.println("Introduce las dimensiones (si es 0 se asignará 20 por defecto):");
-                    
+                    int width,
+                     height,
+                     numberAlive;
+                    Simulation sim;
+
                     do {
                         System.out.print("Ancho: ");
                         width = in.nextInt();
-                        
+
                         if (width > 100) {
                             System.out.println("Las dimensiones no pueden ser mayores que 100.");
                         } else if (width < 0) {
                             System.out.println("Las dimensiones deben ser positivas");
                         }
                     } while (width < 0 || width > 100);
-                    
+
                     do {
                         System.out.print("\nAlto: ");
                         height = in.nextInt();
-                        
+
                         if (height > 100) {
                             System.out.println("Las dimensiones no pueden ser mayores que 100.");
                         } else if (height < 0) {
                             System.out.println("Las dimensiones deben ser positivas.");
                         }
                     } while (height < 0 || height > 100);
-                    
+
                     do {
-                        System.out.print("Introduce el número de bichos iniciales (opcional): ");
+                        System.out.print("Introduce el número de bichos iniciales (0 para aleatorio): ");
                         numberAlive = in.nextInt();
-                        
-                        if (numberAlive > ((width*height)-((int)(width*height*0.01)))) {
+
+                        if (numberAlive > ((width * height) - ((int) (width * height * 0.01)))) {
                             System.out.println("El número es demasiado grande.");
                         } else if (numberAlive < 1) {
-                            System.out.println("El número no puede ser menor que uno.");
+                            System.out.println("El número debe ser positivo.");
                         }
-                    } while (numberAlive > ((width*height)-((int)(width*height*0.01))));
+                    } while (numberAlive > ((width * height) - ((int) (width * height * 0.01))));
+
+                    if (width == 0 && height == 0 && numberAlive == 0) {
+                        sim = new Simulation();
+                    } else if (width != 0 && height != 0) {
+                        sim = new Simulation(width, height);
+                    } else if (numberAlive != 0) {
+                        sim = new Simulation(width, height, numberAlive);
+                    }
+
                     break;
             }
+
         } while (option != 'S');
     }
 
